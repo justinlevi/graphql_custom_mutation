357fe71de3df49bb2fa12f818d97cecaaf2f0d67
Justin Winter <justinlevi@gmail.com>
2017-12-20 15:04:34 -0500

    Delete Page

--- src/Plugin/GraphQL/Mutations/DeletePage.php
+++ src/Plugin/GraphQL/Mutations/DeletePage.php
@@ -4,7 +4,6 @@ namespace Drupal\custom_graphql_mutation\Plugin\GraphQL\Mutations;
 
 
 use Drupal\graphql\Annotation\GraphQLMutation;
-use Drupal\graphql\Plugin\GraphQL\InputTypes\InputTypePluginBase;
 use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\DeleteEntityBase;
 
 

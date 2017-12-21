b2f03c0f3ef2e5fdc6f7caf9252f407cbd79f687
Justin Winter <justinlevi@gmail.com>
2017-12-21 10:20:38 -0500

    The method signature changed?? Super bizarre...

--- src/Plugin/GraphQL/Mutations/AddPage.php
+++ src/Plugin/GraphQL/Mutations/AddPage.php
@@ -4,8 +4,10 @@ namespace Drupal\custom_graphql_mutation\Plugin\GraphQL\Mutations;
 
 
 use Drupal\graphql\Annotation\GraphQLMutation;
+use Drupal\graphql\GraphQL\Type\InputObjectType;
 use Drupal\graphql\Plugin\GraphQL\InputTypes\InputTypePluginBase;
 use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\CreateEntityBase;
+use Youshido\GraphQL\Execution\ResolveInfo;
 
 
 /**
@@ -28,12 +30,10 @@ class AddPage extends CreateEntityBase {
   /**
    * {@inheritdoc}
    */
-  protected function extractEntityInput(array $inputArgs, InputTypePluginBase $inputType) {
-
+  protected function extractEntityInput(array $inputArgs, InputObjectType $inputType, ResolveInfo $info) {
     return [
       'title' => $inputArgs['title'],
       'body' => $inputArgs['body']
     ];
   }
-
 }
--- src/Plugin/GraphQL/Mutations/UpdatePage.php
+++ src/Plugin/GraphQL/Mutations/UpdatePage.php
@@ -4,8 +4,10 @@ namespace Drupal\custom_graphql_mutation\Plugin\GraphQL\Mutations;
 
 
 use Drupal\graphql\Annotation\GraphQLMutation;
+use Drupal\graphql\GraphQL\Type\InputObjectType;
 use Drupal\graphql\Plugin\GraphQL\InputTypes\InputTypePluginBase;
 use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\UpdateEntityBase;
+use Youshido\GraphQL\Execution\ResolveInfo;
 
 
 /**
@@ -26,11 +28,11 @@ use Drupal\graphql_core\Plugin\GraphQL\Mutations\Entity\UpdateEntityBase;
  */
 class UpdatePage extends UpdateEntityBase {
 
+
   /**
    * {@inheritdoc}
    */
-  protected function extractEntityInput(array $inputArgs, InputTypePluginBase $inputType) {
-
+  protected function extractEntityInput(array $inputArgs, InputObjectType $inputType, ResolveInfo $info) {
     return [
       'title' => $inputArgs['title'],
       'body' => $inputArgs['body']
